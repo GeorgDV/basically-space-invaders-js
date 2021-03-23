fc2d2d79df9c1a635814f108f314b8b2997e45c7
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-03-23 09:54:14 +0200

    Add moving enemies and movable player

--- src/main.js
+++ src/main.js
@@ -1,17 +1,31 @@
-var templates = require('./templates.json');
+var templates = require('./templates.json'); // Sprite templates.
 
-var pxLength = 10;
+var pxLength = 10; // Side length of a single "pixel".
+var spriteWidth = pxLength * 5; // Width of a sprite.
+var spriteHeigth = pxLength * 4; // Height of a sprite.
 
-window.addEventListener('load', (event) => {
+var padding = pxLength * 2; // Padding from the edge of the canvas.
+
+var timerInterval = 500; // Timer interval for movement.
+var moveDirection = 'R';
+
+var player = {}; // Player "sprite" object.
+var enemies = []; // Enemies "sprites" objects.
+
+window.addEventListener('keydown', (event) => handleKeyDown(event), false);
+
+window.addEventListener('load', () => {
   initCanvas();
+  initEnemies();
+  initPlayer();
 
-  let robot1 = createSprite('space_ship_1', 200, 200);
-  drawSprite(robot1, '#ffffff');
+  initEnemiesTimer();
+  initPlayerTimer();
 });
 
 function initCanvas() {
   canvas = document.querySelector('#canvas');
-  context = canvas.getContext("2d");
+  context = canvas.getContext('2d');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 }
@@ -32,6 +46,10 @@ function createSprite(id, startX, startY,) {
       start: startY,
       current: startY,
     },
+    velocity: {
+      x: 0,
+      y: 0,
+    },
   }
 }
 
@@ -41,7 +59,7 @@ function drawSprite(sprite, fillStyle) {
       if (value === 1) {
         context.beginPath();
         context.rect(sprite.x.current, sprite.y.current, pxLength, pxLength);
-        context.fillStyle = fillStyle;
+        context.fillStyle = fillStyle ? fillStyle : '#ffffff';
         context.fill();
         context.closePath();
       }
@@ -50,4 +68,80 @@ function drawSprite(sprite, fillStyle) {
     sprite.x.current = sprite.x.start;
     sprite.y.current += pxLength;
   });
+  sprite.y.current = sprite.y.start;
+}
+
+function initEnemies() {
+  let rows = Math.floor((canvas.height / 3) / (spriteWidth));
+  let columns = Math.floor((canvas.width) / (spriteHeigth + pxLength * 5));
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < columns; col++) {
+      //  width or length  +  (margin)     * X/Y postition + padding from edge
+      let x = (spriteWidth + pxLength * 2) * col + padding;
+      let y = (spriteHeigth + pxLength * 2) * row + padding;
+      let sprite = createSprite('space_ship_1', x, y);
+      drawSprite(sprite);
+      enemies.push(sprite);
+    }
+  }
+}
+
+function initPlayer() {
+  let y = canvas.height - (spriteHeigth + padding);
+  let x = padding;
+  player = createSprite('player', x, y);
+  drawSprite(player);
+}
+
+function initEnemiesTimer() {
+  setInterval(() => {
+    context.clearRect(0,0, canvas.width, canvas.height - (spriteHeigth * 2));
+    enemies.forEach((sprite) => {
+      switch (moveDirection) {
+        case 'R':
+          if (sprite.x.current < (canvas.width - spriteWidth - padding)) {
+            sprite.x.current += pxLength;
+            sprite.x.start = sprite.x.current;
+          } else  {
+            //sprite.y.start += pxLength;
+            moveDirection = 'L';
+          }
+          drawSprite(sprite);
+          break;
+        case 'L':
+          if ((sprite.x.current) > (0 + padding)) {
+            sprite.x.current -= pxLength;
+            sprite.x.start = sprite.x.current;
+          } else {
+            //sprite.y.start += pxLength;
+            moveDirection = 'R';
+          }
+          drawSprite(sprite);
+          break;
+      }
+    })
+  }, timerInterval);
+}
+
+function initPlayerTimer() {
+  setInterval(() => {
+    context.clearRect(0, canvas.height - (spriteHeigth * 2), canvas.width, canvas.height);
+    drawSprite(player);
+  }, 10);
+}
+
+function handleKeyDown(event) {
+  switch(event.keyCode) {
+    case 37: 
+      player.x.start -= pxLength;
+      player.x.current -= pxLength;
+      break;
+    case 39:
+      player.x.start += pxLength;
+      player.x.current += pxLength;
+      break;
+    case 32:
+      console.log('shoot');
+      break;
+  }
 }
