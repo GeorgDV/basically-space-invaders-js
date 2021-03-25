ee43be06d687019b121f26c3ce2128496b583838
Georg Daniel Vahtramäe <48998862+GeorgDV@users.noreply.github.com>
2021-03-25 21:18:15 +0200

    Add invaders shoot ability

--- src/main.js
+++ src/main.js
@@ -16,12 +16,16 @@ let synth = new Tone.Synth().toDestination();
 const templates = require('./templates.json');
 
 let player = {}; // Player "sprite" object.
+let bullets = []; // Player bullet "sprite" objects.
 let invaders = []; // Invaders "sprites" objects.
-let bullets = []; // Bullet "sprite" objects.
+let invaderBullets = []; // Invader bullet "sprite" objects.
+let invaderRows; // Number of ROWS of invaders. (scalable)
+let invaderCols; // Number of COLUMNS of invaders. (scalable)
+let invadersFront = [] // Front line of invaders (shooting invaders).
 
 // Game variables.
-let isGameStarted = false;
-let ID = -1; // Id increment for ingame objects.
+let hasGameStarted = false;
+let ID = 0; // Id increment for ingame objects.
 
 const px = 5; // Size a single "pixel".
 const spriteWidth = px * 9; // Width of a default model sprite. (pixel * 9)
@@ -31,17 +35,17 @@ const padding = 20; // Space from the edge of the canvas.
 
 // Timer objects.
 let timers = {
-  enemyTimer: {
+  invaderSpeedTimer: {
     value: null,
-    interval: 650,
+    interval: 700,
   },
   playerTimer: {
     value: null,
     interval: 20,
   },
-  playerBulletTimer: {
+  invaderBulletTimer: {
     value: null,
-    interval: 15,
+    interval: 30,
   },
 }
 
@@ -55,13 +59,16 @@ let invadersStep = px * 3;
 let invadersStepsCount = 0;
 
 let isShootKeyPressed = false;
-window.addEventListener('keydown', (event) => handleKeyDown(event), false);
+window.addEventListener('keydown', (event) => {
+  if (!hasGameStarted) return;
+  handleKeyDown(event);
+});
+
 window.addEventListener('click', () => {
-  if (!isGameStarted) {
-    StartAudioContext(Tone.getContext());
-    initGame();
-    isGameStarted = true;
-  }
+  if (hasGameStarted) return;
+  StartAudioContext(Tone.getContext());
+  initGame();
+  hasGameStarted = true;
 });
 
 window.onload = () => {
@@ -83,7 +90,7 @@ function initGame() {
 
   initEnemiesTimer();
   initPlayerTimer();
-  initBulletsTimer();
+  setTimeout(() => initInvaderBulletTimer(), 3000);
 }
 
 function initCanvas() {
@@ -99,7 +106,8 @@ function initAudio() {
   Tone.setContext(audioContext);
 }
 
-function createSprite(id, startX, startY,) {
+// Used for creating sprite objects.
+function createSprite(id, startX, startY) {
   let template = templates.find((template) => template.id === id);
   if (!template) {
     throw new Error(`No sprite template for '${id}' was found.`);
@@ -126,6 +134,20 @@ function createSprite(id, startX, startY,) {
   }
 }
 
+// Used for creating arc objects.
+function createArc(x, y) {
+  return {
+    id: ID++,
+    template: {
+      id: 'arc',
+      data: null,
+    },
+    x: x,
+    y: y,
+    angle: 90, // Default value 180 degrees.
+  }
+}
+
 function updateSpriteTemplate(oldSprite, newTemplateId) {
   let template = templates.find((template) => template.id === newTemplateId);
   if (!template) {
@@ -141,6 +163,7 @@ function updateSpriteTemplate(oldSprite, newTemplateId) {
   }
 }
 
+// Used for drawing sprite objects using template data.
 function drawSprite(sprite, fillStyle, pxWidth, pxHeight) {
   sprite.template.data.forEach((row) => {
     row.forEach((value) => {
@@ -159,22 +182,38 @@ function drawSprite(sprite, fillStyle, pxWidth, pxHeight) {
   sprite.y.current = sprite.y.start;
 }
 
+// Used for drawing arcs.
+function drawArc(arc, radius, fillStyle) {
+  ctx.beginPath();
+  ctx.arc(arc.x, arc.y, radius ? radius : px, 0, 2 * Math.PI);
+  ctx.fillStyle = fillStyle ? fillStyle : '#ffffff';
+  ctx.fill();
+  ctx.closePath();
+}
+
 function initInvaders() {
-  let rows = Math.floor((canvas.height / 3) / (spriteWidth * 1.5));
-  let columns = Math.floor((canvas.width) / (spriteHeigth  * 3));
-  for (let row = 0; row < rows; row++) {
+  invaderRows = Math.floor((canvas.height / 3) / (spriteWidth * 1.5));
+  invaderCols = Math.floor((canvas.width) / (spriteHeigth  * 3));
+  for (let row = 0; row < invaderRows; row++) {
     let id = 'invader_2-1';
+
+    // First row gets different sprites.
     if (row === 0) {
       id = 'invader_1-1';
     }
 
-    for (let col = 0; col < columns; col++) {
+    for (let col = 0; col < invaderCols; col++) {
       //  width or length + (margin) * X/Y postition + padding from edge.
       let x = (spriteWidth + px * 3) * col + padding;
       let y = (spriteHeigth + px * 3) * row + padding / 2;
-      let sprite = createSprite(id, x, y);
-      invaders.push(sprite);
-      drawSprite(sprite);
+      let invader = createSprite(id, x, y);
+      invaders.push(invader);
+      drawSprite(invader);
+
+      // Last row of invaders are initally front line invaders.
+      if (row === (invaderRows - 1)) {
+        invadersFront.push(invader);
+      }
     }
   }
 }
@@ -187,27 +226,75 @@ function initPlayer() {
 }
 
 function initEnemiesTimer() {
-  timers.enemyTimer.value = setInterval(() => {
+  timers.invaderSpeedTimer.value = setInterval(() => {
     updateMoveDirection();
     updateInvaders();
-  }, timers.enemyTimer.interval);
+  }, timers.invaderSpeedTimer.interval);
 }
 
 function initPlayerTimer() {
   timers.playerTimer.value = setInterval(() => {
     updatePlayer();
-  }, timers.playerTimer.interval);
-}
 
-function initBulletsTimer() {
-  timers.playerBulletTimer.value = setInterval(() => {
-    if (!bullets.length) {
-      return;
-    } else {
+    // Control bullets fired by player.
+    if (bullets.length > 0) {
       detectBulletAndInvaderCollision();
       updateBullets();
     }
-  }, timers.playerBulletTimer.interval);
+  }, timers.playerTimer.interval);
+}
+
+function initInvaderBulletTimer() {
+  timers.invaderBulletTimer.value = setInterval(() => {
+    // Choose if to shoot or not.
+    let random = getRandomInteger(1, invaders.length === 1 ? 8 : 24);
+    if (random === 1) {
+      // Get random front line invader to shoot.
+      let index = Math.floor(Math.random() * invadersFront.length);
+      invaderShoot(invadersFront[index]);
+    }
+    
+    if (invaderBullets.length > 0) {
+      updateInvaderBullets();
+    }
+  }, timers.invaderBulletTimer.interval);
+}
+
+function updateInvaderBullets() {
+  invaderBullets.forEach((bullet) => {
+    ctx.clearRect(bullet.x - px, bullet.y - px, px*2, px*2);
+    //bullet.x += Math.cos(deg2rad(bullet.angle))*6; // Uncomment this when there is horizonal movement.
+    bullet.y += Math.sin(deg2rad(bullet.angle))*6;
+
+    // Bullet hits wall.
+    if (bullet.y > canvas.height) {
+      invaderBullets = invaderBullets.filter((item) => item.id !== bullet.id);
+    } else {
+      drawArc(bullet, px - 2, '#dddddd');
+    }
+  });
+}
+
+function updateInvadersFront(invaderToReplace) {
+  // If killed invader is not from front line, dont do anything.
+  if (!invadersFront.some((invader) => invader.id === invaderToReplace.id)) {
+    return;
+  }
+
+  // Remove killed invader from front line.
+  invadersFront = invadersFront.filter((invader) => invader.id !== invaderToReplace.id)
+  
+  // Find replacement invader from previous line.
+  let index = invaderToReplace.id - invaderCols;
+  for (let i = index; i >= invaders[0].id; i -= invaderCols) {
+    // Stepping back line after line. If there is any invader
+    // from previous lines, it gets replaced by that.
+    let invaderBehind = invaders.find((invader) => invader.id === i)
+    if (invaderBehind) {
+      invadersFront.push(invaderBehind);
+      return;
+    }
+  }
 }
 
 function updateMoveDirection() {
@@ -256,7 +343,13 @@ function updateInvaders() {
         break;
     }
     
-    // Demo attempt at animating invaders movement.
+    // If invader is from front line we update it.
+    let indexOfFrontInvader = invadersFront.map((item) => item.id).indexOf(invader.id);
+    if (indexOfFrontInvader !== -1) {
+      invadersFront[indexOfFrontInvader] = invader;
+    }
+    
+    // Animating invaders movement.
     let index;
     switch (invader.template.id) {
       case 'invader_1-1':
@@ -326,11 +419,27 @@ function movePlayer() {
 }
 
 function shoot() {
-  let bullet = createSprite('bullet', player.x.start + Math.floor(spriteWidth / 2 - 2), player.y.start - Math.floor(spriteHeigth / 2));
-  bullet.velocity.y = -1;
+  let x = player.x.start + Math.floor(spriteWidth / 2 - 2);
+  let y = player.y.start - Math.floor(spriteHeigth / 2);
+  let bullet = createSprite('bullet', x, y);
   bullets.push(bullet);
   drawSprite(bullet, '#77ff00', px - 3);
   playLaserSound();
+}
+
+function invaderShoot(invader) {
+  let x = invader.x.start + (spriteWidth / 2);
+  let y = invader.y.start + spriteHeigth + (px * 3);
+  let bullet = createArc(x, y);
+
+  // Calculate shooting angle. Not working 100% correctly atm.
+  // let a = player.x.start + (spriteWidth / 2) - bullet.x;
+  // let b = player.y.start + (spriteHeigth / 2) - bullet.y;
+  // let angleRad = Math.atan(Math.abs(a / b));
+  // bullet.angle = rad2deg(angleRad) + 90;
+
+  invaderBullets.push(bullet);
+  drawArc(bullet, px - 2, '#dddddd');
 }
 
 function updateBullets() {
@@ -344,7 +453,7 @@ function updateBullets() {
 
     // Bullet hits wall.
     if (bullet.y.start < 0) {
-      bullet = updateSpriteTemplate(bullet, 'bullet_hit_wall');
+      bullet = updateSpriteTemplate(bullet, 'bullet_hit_wall_1');
       bullet.x.start -= (px * 5) - (px * 3); // Move new sprite to middle of bullet hit.
       bullet.x.current = bullet.x.start;
       drawSprite(bullet, '#c0ff8a', px - 3, px - 3);
@@ -377,27 +486,29 @@ function detectBulletAndInvaderCollision() {
         bulletY <= invaderBottomY &&
         bulletY >= invaderTopY) {
 
+        // Update front line invaders.
+        updateInvadersFront(invader);
+
         // Remove bullet.
         ctx.clearRect(bullet.x.start, bullet.y.start, px - 3, px * 4);
         bullets = bullets.filter((item) => item.id !== bullet.id);
 
         // Remove invader and play explosion.
-        ctx.clearRect(invader.x.start, invader.y.start - px, spriteWidth, spriteHeigth + px);
         invader = updateSpriteTemplate(invader, 'explosion');
+        ctx.clearRect(invader.x.start, invader.y.start - px, spriteWidth, spriteHeigth + px);
         drawSprite(invader, '#ffffff', px - 2, px - 2);
         invaders = invaders.filter((item) => item.id !== invader.id);
         // Clear explosion.
         setTimeout(() => ctx.clearRect(invader.x.start, invader.y.start - px, spriteWidth, spriteHeigth + px), 75);
 
-
         // Step to decrease timer = 600 / number of invaders
-        let timerStep = Math.floor((timers.enemyTimer.interval - 50) / (invaders.length));
+        let timerStep = Math.floor((timers.invaderSpeedTimer.interval - 100) / (invaders.length));
 
-        // Update timer.
-        timers.enemyTimer.interval -= timerStep;
+        // Update invaders timer.
+        timers.invaderSpeedTimer.interval -= timerStep;
         initEnemiesTimer();
         if (!invaders.length) {
-          clearInterval(timers.enemyTimer.value);
+          clearInterval(timers.invaderSpeedTimer.value);
           setTimeout(() => alert('You Won!'), 500);
         }
       }
@@ -415,4 +526,14 @@ function playStepSound() {
   } else if (invadersStepsCount % 2 > 0) {
     synth.triggerAttackRelease("C2", 0.05);
   }
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+function rad2deg(rad) {
+  return rad * (180/ Math.PI);
+}
+function getRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
